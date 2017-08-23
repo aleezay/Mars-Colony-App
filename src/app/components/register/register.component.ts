c75d62774b73fb458badeb1e11b4ebe7dde80820
aleezay <aleezayh@hotmail.com>
2017-08-23 15:05:52 -0700

    added validation to registration component form, need to work on report component form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ColonistService } from '../../services/colonist';
+import { NewColonist } from '../../models/colonist';
 import { JobService } from '../../services/job';
 import {Job} from '../../models/job';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -19,9 +19,17 @@ export class RegisterComponent implements OnInit {
   jobs: Job[] = [];
 
 registerForm = new FormGroup({
-         name: new FormControl(''),
-          age: new FormControl(''),
-       job_id: new FormControl('')
+         name: new FormControl('', 
+         [Validators.required,
+         Validators.maxLength(100),
+         Validators.minLength(2),
+         this.noNumbers('')
+         ]),
+          age: new FormControl('', [Validators.required,
+          Validators.max(9999),
+          Validators.min(0),
+          ]),
+       job_id: new FormControl('', [Validators.required])
 });
   
   constructor(
@@ -33,4 +41,24 @@ registerForm = new FormGroup({
     this.jobs = await this.jobService.getJobs();
     console.log(this.jobs);
   }
+
+async registerColonist(){
+   const newColonist: NewColonist = {
+     name: this.registerForm.get('name').value,
+     age: this.registerForm.get('age').value,
+     job_id: this.registerForm.get('job_id').value
+   };
+
+   const colonist = await this.colonistService.registerColonist(newColonist);
+   console.log('Colonists eat marsbars', colonist );
+ }
+
+ private noNumbers(validNameRegex): ValidatorFn {
+   return(control): {[key:string] : any } => { //this function returns an object. cutom validator just copy-paste. all look the same.
+const forbiddenName = validNameRegex.test(control.value);
+return forbiddenName ? { 'forbiddenName' : { value: control.value } } : null;
+   };
+ }
+
 }
+//rejex that returns true if it finds numbers in a string
--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AlienService } from '../../services/alien';
+import { FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
+
 
 
 @Component({
@@ -19,3 +21,52 @@ async ngOnInit(){
   console.log(aliens);
 }
 }
+  
+
+
+//   jobs: Job[] = [];
+
+// registerForm = new FormGroup({
+//          name: new FormControl('', 
+//          [Validators.required,
+//          Validators.maxLength(100),
+//          Validators.minLength(2),
+//          this.noNumbers('')
+//          ]),
+//           age: new FormControl('', [Validators.required,
+//           Validators.max(9999),
+//           Validators.min(0),
+//           ]),
+//        job_id: new FormControl('', [Validators.required])
+// });
+  
+//   constructor(
+//     private jobService: JobService,
+//     private colonistService: ColonistService
+//   ) {}
+
+//   async ngOnInit() {
+//     this.jobs = await this.jobService.getJobs();
+//     console.log(this.jobs);
+//   }
+
+// async registerColonist(){
+//    const newColonist: NewColonist = {
+//      name: this.registerForm.get('name').value,
+//      age: this.registerForm.get('age').value,
+//      job_id: this.registerForm.get('job_id').value
+//    };
+
+//    const colonist = await this.colonistService.registerColonist(newColonist);
+//    console.log('Colonists eat marsbars', colonist );
+//  }
+
+//  private noNumbers(validNameRegex): ValidatorFn {
+//    return(control): {[key:string] : any } => { //this function returns an object. cutom validator just copy-paste. all look the same.
+// const forbiddenName = validNameRegex.test(control.value);
+// return forbiddenName ? { 'forbiddenName' : { value: control.value } } : null;
+//    };
+//  }
+
+// }
+// //rejex that returns true if it finds numbers in a string
